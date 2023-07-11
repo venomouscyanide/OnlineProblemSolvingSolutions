a92f04b25f7381c7c471521845dbd7ee32c02e07
Paul Louis <paul.louis@ontariotechu.net>
2023-07-11 12:32:52 -0400

    Transpose

---
+++ LeetCode/Siemens/transpose.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        new_matrix = [[0] * len(matrix) for _ in range(len(matrix[0]))]
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                new_matrix[j][i] = matrix[i][j]
+
+        return new_matrix
